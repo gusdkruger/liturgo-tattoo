cc8c4fe68a2c2aae6f5ecb7bc18f0553ab69ccdd
Gustavo Kruger <gusdkruger@gmail.com>
2024-07-09 17:06:40 -0300

    Added nav links in header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+const navLinks = document.querySelectorAll('.header__nav__link');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', function() {
+        const attribute = navLink.getAttribute('data-link-to-class');
+        const element = document.querySelector(`.${attribute}`);
+        if(element) {
+            const bodyRect = document.body.getBoundingClientRect();
+            const elementRect = element.getBoundingClientRect();
+            window.scrollTo(0, elementRect.top - bodyRect.top - 90);
+        }
+    });
+});
+
 function adjustImgContainerHeight() {
     const imgContainer = document.querySelector('.contact__img-container');
-    if (window.innerWidth >= 900) {
+    if(window.innerWidth >= 900) {
         const form = document.querySelector('.contact__form');
-        if (imgContainer && form) {
+        if(imgContainer && form) {
             imgContainer.style.height = `${form.offsetHeight}px`;
         }
     }
-    else if (imgContainer) {
+    else if(imgContainer) {
         imgContainer.style.height = 'fit-content';
     }
 }
